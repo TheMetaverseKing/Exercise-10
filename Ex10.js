89f7d3da7d6f8e31545c727f574d215f023a6801
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-06 11:35:55 +0500

    Add files via upload

---
+++ Ex10.js
@@ -0,0 +1,3 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does
+console.log("This program is about adding comments in a JS code. Comments are added with //. The colour of the text tunrs Green when comments are detected by VSCode");
